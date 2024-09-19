94bc3f9cb87d0fcfa6ab737bbc402eadb7c03c09
bodsul <bodsul@gmail.com>
2024-09-19 14:41:33 -0700

    add missing script

---
+++ src/tokenizer_utils.py
@@ -0,0 +1,14 @@
+import tiktoken
+import torch
+
+tokenizer = tiktoken.get_encoding("gpt2")
+
+def text_to_token_ids(text, tokenizer):
+    encoded = tokenizer.encode(text)
+    encoded_tensor = torch.tensor(encoded).unsqueeze(0)  # add batch dimension
+    return encoded_tensor
+
+
+def token_ids_to_text(token_ids, tokenizer):
+    flat = token_ids.squeeze(0)  # remove batch dimension
+    return tokenizer.decode(flat.tolist())
